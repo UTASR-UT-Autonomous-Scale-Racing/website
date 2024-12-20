f6d885c7176b38c166c7cc2a4f59f3afa7ad8a45
brownd58 <danieldebrun.brown@mail.utoronto.ca>
2024-12-20 13:43:13 -0500

    Template project commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // CSS Import
+
+function App() {
+  return (
+    <div className="gradient-background">
+      <div className="content">
+        <h1>UTASR</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
