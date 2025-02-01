7f5d48abd099dbc89396a0246760e722ac9ad8af
MohamedEBR <110148669+MohamedEBR@users.noreply.github.com>
2025-01-31 23:32:47 -0500

    Basic Carousel.jsx Completed
    Completed the Carousel formatted we designed in Figma. The width and heights of each grid can be dynamically changed.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,71 +1,202 @@
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Box from '@mui/joy/Box';
-import Typography from '@mui/joy/Typography';
-import Card from '@mui/joy/Card';
-import SimpleContainer from './SimpleContainer';
-
+import * as React from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Box from "@mui/joy/Box";
+import Card from "@mui/joy/Card";
+import SimpleContainer from "./SimpleContainer";
+import { Grid2 } from "@mui/material";
 const data = [
   {
-    src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
-    title: 'Night view',
+    src: "https://images.unsplash.com/photo-1502657877623-f66bf489d236",
+    title: "Night view",
   },
   {
-    src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
-    title: 'Lake view',
+    src: "https://images.unsplash.com/photo-1527549993586-dff825b37782",
+    title: "Lake view",
   },
   {
-    src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
-    title: 'Mountain view',
+    src: "https://images.unsplash.com/photo-1532614338840-ab30cf10ed36",
+    title: "Mountain view",
   },
   {
-    src: 'https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0',
-    title: 'Mountain view',
+    src: "https://www.jessleephotos.com/images/xl/Cascaperefflowers.jpg",
+    title: "Mountain view",
   },
   {
-    src: 'https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0',
-    title: 'Mountain view',
+    src: "https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0",
+    title: "Mountain view",
   },
   {
-    src: 'https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0',
-    title: 'Mountain view',
+    src: "https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0",
+    title: "Mountain view",
   },
   {
-    src: 'https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0',
-    title: 'Mountain view',
+    src: "https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0",
+    title: "Mountain view",
+  },
+  {
+    src: "https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0",
+    title: "Mountain view",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1502657877623-f66bf489d236",
+    title: "Night view",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1527549993586-dff825b37782",
+    title: "Lake view",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1532614338840-ab30cf10ed36",
+    title: "Mountain view",
+  },
+  {
+    src: "https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0",
+    title: "Mountain view",
+  },
+  {
+    src: "https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0",
+    title: "Mountain view",
+  },
+  {
+    src: "https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0",
+    title: "Mountain view",
+  },
+  {
+    src: "https://www.originaltravel.co.uk/travel-blog/showphoto/4103/0",
+    title: "Mountain view",
   },
 ];
 
+const diffHeight = {
+
+  0: {
+    'md': 400,
+    'sm': 300,
+    'xs': 200
+  },
+  1: {
+    'md': 400,
+    'sm': 300,
+    'xs': 200
+  },
+  2: {
+    'md': 400,
+    'sm': 300,
+    'xs': 200
+  },
+  3: {
+    'md': 300,
+    'sm': 200,
+    'xs': 200
+  },
+  4: {
+    'md': 300,
+    'sm': 200,
+    'xs': 200
+  },
+  5: {
+    'md': 300,
+    'sm': 200,
+    'xs': 200
+  }
+
+};
+
+
+const diffSize = {
+  0: { 'md': "50%", 'sm': "90%", 'xs': "90%" },
+  1: { 'md': "22%", 'sm': "44%", 'xs': "43%" },
+  2: { 'md': "22%", 'sm': "44%", 'xs': "43%" },
+  3: { 'md': "27%", 'sm': "44%", 'xs': "90%" },
+  4: { 'md': "27%", 'sm': "44%", 'xs': "43%" },
+  5: { 'md': "40%", 'sm': "44%", 'xs': "43%" }
+};
 export default function Carousel() {
+  const pages = [];
+
+  console.log(`${100 / diffSize[0 % 6]['md']}%`)
+  // Create pages with 6 images each
+  for (let i = 0; i < data.length; i += 6) {
+    pages.push(
+
+        <Grid2 container spacing={2} key={i}
+        sx={{
+          width: "100vw", // Full viewport width
+
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          p: 5,
+          mx: 10
+        }}>
+        {data.slice(i, i + 6).map((item, index) => (
+
+          <Grid2 key={index}  
+          
+          sx={{
+            width: {
+              xs: diffSize[index % 6]['xs'],
+              sm: diffSize[index % 6]['sm'],
+              md: diffSize[index % 6]['md']
+            }
+          }}
+
+           >
+            <Box  
+            component="img"
+            sx={{ 
+
+              height:  {
+                xs : diffHeight[index % 6]['xs'],
+                sm : diffHeight[index % 6]['sm'],
+                md : diffHeight[index % 6]['md'],
+                
+              },
+              objectFit : "cover",
+              width: "100%",
+              borderRadius: 10,
+              transition: "0.3s ease-in-out",
+              "&:hover": {
+                transform: "scale(1.02)"
+              },
+              boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+              }}
+              
+              src={`${item.src}?h=120&fit=crop&auto=format`}
+              alt={item.title}
+
+              />
+       
+          
+          </Grid2>
+        ))}
+      </Grid2>
+      
+    );
+  }
+
   return (
-    <SimpleContainer bgColor={'#FFE8E8'}>
+    <SimpleContainer bgColor="#162E7C">
       <Box
         sx={{
-          display: 'flex',
+          display: "flex",
           gap: 2,
-          py: 2,
-          overflow: 'auto',
-          width: '100%',
-          scrollSnapType: 'x mandatory',
-          alignItems: 'center',
-          '& > *': {
-            scrollSnapAlign: 'center',
+          overflow: "auto",
+          width: "100%",
+          scrollSnapType: "x mandatory",
+          alignItems: "center",
+          overflowX: "auto",
+          overflowY: "hidden",
+          "& > *": {
+            scrollSnapAlign: "center",
+            flexShrink: 0,
           },
-          '::-webkit-scrollbar': { display: 'none' },
+          "::-webkit-scrollbar": { display: "none" },
+          background:" rgb(2,0,36)",
+          background: "linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(22,46,124,1) 43%, rgba(82,118,240,1) 100%)"
         }}
       >
-        {data.map((item) => (
-          <Card key={item.title} ratio="16/9" variant="outlined" sx={{ minWidth: 300 }}>
-            <AspectRatio ratio="1" sx={{ width: '100%', height: 300 }}>
-              <img
-                srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
-                src={`${item.src}?h=120&fit=crop&auto=format`}
-                alt={item.title}
-                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-              />
-            </AspectRatio>
-          </Card>
-        ))}
+        {pages}
       </Box>
     </SimpleContainer>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,52 @@
 import React from 'react';
 import SimpleContainer from './SimpleContainer';
+import car from '../assets/dummycar.jpg';
+import { Box } from '@mui/material';
 
 export const Main = () => {
   return (
     <SimpleContainer bgColor="#fffff">
-      <header>
-        <h1 style={{ fontSize: '3rem' }}>Main</h1>
-        <h2 style={{ fontSize: '2rem' }}>Who we are</h2>
-        <p>aodsbfasdiofjasiufnhasiduf</p>
-        <h2 style={{ fontSize: '2rem' }}>Objectives</h2>
-        <p>aodsbfasdiofjasiufnhasiduf</p>
-      </header>
+      
+      <Box
+       sx={{ 
+        display: 'flex', 
+        flexDirection: 'row',  
+        justifyContent: 'space-between', 
+        alignItems: 'center',
+        minHeight: '750px',
+        width: '100%',
+        px:10,
+        }}
+        >
+           {/* Text Container */}
+      <Box
+      sx={{
+        // width: '100%%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+      }}>
+        <header>
+          <h1 style={{ fontSize: '3rem' }}>Main</h1>
+          <h2 style={{ fontSize: '2rem' }}>Who we are</h2>
+          <p>aodsbfasdiofjasiufnhasiduf</p>
+          <h2 style={{ fontSize: '2rem' }}>Objectives</h2>
+          <p>aodsbfasdiofjasiufnhasiduf</p>
+        </header>
+      </Box>
+
+      {/* Image Container */}
+      <Box
+      component="img"
+      src={car}
+      sx={{
+        width: "500px"
+      }}/>
+
+
+      </Box>
+     
+      
     </SimpleContainer>
   );
 }
