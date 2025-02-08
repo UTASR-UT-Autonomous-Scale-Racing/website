d792cde15f5b008ccb14f98ccce8ef734a551509
Daniel Stevanus <danielstevanus88@gmail.com>
2025-02-08 03:06:57 -0500

    added team info

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,37 +1,52 @@
 import React from 'react';
 import SimpleContainer from './SimpleContainer';
 import '../styles/AboutUs.css';
+import { Box, Typography } from '@mui/material';
 export const AboutUs = () => {
   return (
-  <div className='about-us' style={{padding: '100px 0px'}}>
-    <SimpleContainer >
-      <header>
-        <h1 style={{ fontSize: '3rem', textAlign:'center' }}>Our Team</h1>
-      </header>
+    <Box className='about-us' color='white' backgroundColor='#2d48a6' sx={{ padding: '100px 0px' }}>
+      <SimpleContainer>
+        <header>
+          <Typography
+            variant="h4"
+            sx={{
+              textAlign: 'center',
+              fontSize: '2.5rem',
+              fontWeight: '600',
+              color: 'white',
+              mb: 3
+            }}
+          >
+            Our Team
+          </Typography>
+        </header>
 
-      <div className="flex-container">
-      <div className="team-member">
-        <img src="https://img.freepik.com/premium-photo/diversity-race-ethnicity-people-concept-international-group-happy-smiling-men-women-showing-thumbs-up-white_380164-164822.jpg" 
-          alt="Web Dev Team" className="team-image" />
-        <p>Web Dev Team</p>
-      </div>
-      <div className="team-member">
-        <img src="https://img.freepik.com/premium-photo/diversity-race-ethnicity-people-concept-international-group-happy-smiling-men-women-showing-thumbs-up-white_380164-164822.jpg" 
-          alt="AA Team" className="team-image" />
-        <p>AA Team</p>
-      </div>
-      <div className="team-member">
-        <img src="https://img.freepik.com/premium-photo/diversity-race-ethnicity-people-concept-international-group-happy-smiling-men-women-showing-thumbs-up-white_380164-164822.jpg" 
-          alt="Hardware Team" className="team-image" />
-        <p>Hardware Team</p>
-      </div>
+        <Box display="flex" justifyContent="center" flexDirection={{ xs: 'column', md: 'row' }}>
+          <Box className="team-member" textAlign="center" sx={{margin: '40px 20px' }}  width={{xs:'100%', md:'45%'}}>
+            
+            <Box color="white" display="flex" flexDirection="column" alignItems="center" justifyContent={{ xs: 'center', md: 'flex-start' }}>
+            <img style={{margin: "25px 0px"}} src="https://img.freepik.com/premium-photo/diversity-race-ethnicity-people-concept-international-group-happy-smiling-men-women-showing-thumbs-up-white_380164-164822.jpg"
+              alt="Web Dev Team" className="team-image" />
+              <Typography variant='p'  fontSize={{sx:"1.5rem", md:"3rem"}}>Hardware Team</Typography>
 
-      <style jsx>{`
-        
-      `}</style>
-      </div>
-    </SimpleContainer>
-    </div>
+              <Typography width="80%"  fontSize={{sx:"1.2rem", md:"1.5rem"}}>Handles mechanical design, aerodynamics, electronics, and firmware. Works on CAD modeling, PCB design, sensor integration, and real-time control.
+                </Typography>
+            </Box>
+          </Box>
+          <Box className="team-member" textAlign="center" sx={{margin: '40px 20px' }} width={{xs:'100%', md:'45%'}}>
+          <Box color="white" display="flex" flexDirection="column" alignItems="center" justifyContent={{ xs: 'center', md: 'flex-start' }}>
+            <img style={{margin: "25px 0px"}} src="https://img.freepik.com/premium-photo/diversity-race-ethnicity-people-concept-international-group-happy-smiling-men-women-showing-thumbs-up-white_380164-164822.jpg"
+              alt="Web Dev Team" className="team-image" />
+              <Typography variant='p' fontSize={{sx:"1.5rem", md:"3rem"}}>Software Team</Typography>
+
+              <Typography width="80%"  fontSize={{sx:"1.5rem", md:"1.5rem"}}>Focuses on data processing, machine learning, and system optimization. Cleans and preprocesses sensor data, builds AI models, 
+                and optimizes real-time performance.
+                </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </SimpleContainer>
+    </Box>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,70 +1,87 @@
 import React from 'react';
 import SimpleContainer from './SimpleContainer';
 import car from '../assets/dummycar.jpg';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 export const Main = () => {
+  const [phone, setPhone] = React.useState(false);
+  
+  React.useEffect(() => {
+    setPhone(window.innerWidth < 700);
+    
+    const handleResize = () => {
+      setPhone(window.innerWidth < 700);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const textStyle = {
+    fontFamily: 'Poppins, sans-serif',
+  };
+
+
   return (
-    <div style={{padding: '100px 0px'}}>
-    <SimpleContainer bgColor="#fffff">
-      <header>
-        <h1 style={{ fontSize: '3rem', textAlign:'center', fontWeight:'bold'
-         }}>Welcome to UTASR</h1>
-      </header>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: {
-            md: 'row',
-            xs: "column"
-          },
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '100%',
-          padding: 0,
-        }}
-      >
-        
-        {/* Text Container */}
+    <Box sx={{ padding: { xs: '50px 20px', md: '100px' } }}>
+      <SimpleContainer bgColor="#fffff">
+        <header>
+          {!phone && <h1 style={{ fontSize: '3rem', textAlign: 'center', fontWeight: 'bold', ...textStyle }}>Welcome to UTASR</h1>}
+        </header>
         <Box
           sx={{
-            // width: '100%%',
             display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-evenly',
-          }}>
-          <header style={{ textAlign: 'justify', padding: '0px 20px' }}>
-            <h2 style={{ fontSize: '2rem' }}>Who we are</h2>
-            <p>University of Toronto Anonymous Scale Racing (UTASR) is a forward-thinking design team dedicated to developing scalable,
-              learning-based algorithms for RC car control. Our mission is to create accessible projects that offer hands-on testing and
-              practical experience, empowering our members with real-world engineering skills.
-            </p>
-            <p>We host workshops, collaborative events, and on-site testing sessions to foster a community of innovation. Our activities
-              are open to all engineering students eager to apply their skills in a dynamic, engaging environment.
-            </p>
-            <p>Join us to explore algorithm development, design, and the thrill of RC racing while contributing to a vibrant campus life at UofT.</p>
-            
-            <h2 style={{ fontSize: '2rem', marginTop:"10px" }}>Objectives</h2>
-            <ul style={{ listStyleType: 'circle', paddingLeft: '20px' }}> 
-              <li>Develop cutting-edge AI and robotics solutions.</li>
-              <li>Foster collaboration and learning among students.</li>
-              <li>Compete and excel in autonomous racing competitions.</li>
-            </ul>
+            flexDirection: {
+              md: 'row',
+              xs: 'column'
+            },
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+            padding: 0,
+          }}
+        >
+          {/* Text Container */}
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-evenly',
+              '@media (max-width: 600px)': {
+                padding: '20px',
+              },
+            }}>
+            <header style={{ textAlign: 'justify', padding: '0px 20px', ...textStyle }}>
+              <h2 style={{ fontSize: '2rem', ...textStyle }}> <b> Who we are </b></h2>
+              <p><b>University of Toronto Anonymous Scale Racing (UTASR)</b> is a forward-thinking design team dedicated to developing scalable,
+                learning-based algorithms for RC car control. Our mission is to create accessible projects that offer hands-on testing and
+                practical experience, empowering our members with real-world engineering skills.
+              </p>
+              <p>We host workshops, collaborative events, and on-site testing sessions to foster a community of innovation. Our activities
+                are open to all engineering students eager to apply their skills in a dynamic, engaging environment.
+              </p>
+              <p>Join us to explore algorithm development, design, and the thrill of RC racing while contributing to a vibrant campus life at UofT.</p>
+
+              <h2 style={{ fontSize: '2rem', marginTop: "10px", ...textStyle }}><b>Our Objectives</b></h2>
+              <ul style={{ listStyleType: 'circle', paddingLeft: '20px', ...textStyle }}>
+                <li>Develop cutting-edge AI and robotics solutions.</li>
+                <li>Foster collaboration and learning among students.</li>
+                <li>Compete and excel in autonomous racing competitions.</li>
+              </ul>
             </header>
           </Box>
-        <Box
-          component="img"
-          src={car}
-          sx={{
-            width: "500px"
-          }} />
-
-
-      </Box>
-
-
-    </SimpleContainer>
-    </div>
+          <Box
+            component="img"
+            src={car}
+            sx={{
+              width: "500px",
+              '@media (max-width: 600px)': {
+                width: '100%',
+              },
+            }} />
+        </Box>
+      </SimpleContainer>
+    </Box>
   );
 }
 
