5833499f1601170e63132d6e196e9e19def9eee4
MohamedEBR <ebraheemohamed26@gmail.com>
2025-08-26 19:51:40 -0400

    fixed white space bugs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <div>
+    <div className="App" style={{ backgroundColor: '#0b1024', minHeight: '100vh' }} > 
       <HomePage />
     </div>
   );
